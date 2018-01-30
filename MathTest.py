b1f44a94108d004c087253c6f5e3e6781e682dca
Mike Drioel <mikedrioel@hotmail.com>
2018-01-30 19:47:47 +0100

    Update
    Update

---
+++ MathTest.py
@@ -0,0 +1,16 @@
+import math
+
+a=float(input("Geef lengte van overstaande zijde"))
+b=float(input("Geef lengte van overstaande zijde"))
+list=[]
+list.append(a)
+list.append(b)
+print list[-1]
+c=math.sqrt(a**2+b**2)
+
+print "Lengte van schuine zijde:",c
+
+
+print "To reset the tabel type y/n"
+
+
